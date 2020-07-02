cbbe88fc5afc44d7b3fbf0407e1e4676f759b7c1
DimaDP <dimadp95@gmail.com>
2020-07-02 14:07:06 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ffde3675347713e2155b1328e0cfa1a1.js"
+  "./precache-manifest.1b15488e42a74904ceec38e34612c105.js"
 );
 
 self.addEventListener('message', (event) => {
